dc53a26dadfb23c565f4bf84044c50d073c0d541
Diego <jdiego122253@gmail.com>
2015-08-10 16:33:04 +0200

    Añadidos Fragments Programa

---
+++ app/src/main/java/com/jldes/fiestassanromandehornija/BlankFragment16.java
@@ -0,0 +1,20 @@
+package com.jldes.fiestassanromandehornija;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BlankFragment16 extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_programa, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/jldes/fiestassanromandehornija/BlankFragment18.java
@@ -0,0 +1,20 @@
+package com.jldes.fiestassanromandehornija;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BlankFragment18 extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_blank_fragment18, container, false);
+        return rootView;
+    }
+}
